b0a3d233578de5c5bd456714a68f132fe633ffa2
Jeimy Mota <Jeimy Mota>
2017-05-29 22:06:37 -0400

    origin files

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+//Objective: Use Jquery and Javascript to create a trivia game. Html components are to be modified by jquery. 
+//If player chooses the right answer, there must be an alert congratulating them. Once the player clicks okay, the next question pops up. 
+//If player chooses the wrong answer, there must be an alert telling the it's wrong. Once the player clicks okay, the next question pops up.
+//At the end of the game, there should be the number of correct answers, the number of incorrect answers and an option to restart the game. 
